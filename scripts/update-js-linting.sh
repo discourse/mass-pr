9b59c7fbb3510f88c40e8fce394b677e46f458b3
Jarek Radosz <jradosz@gmail.com>
2023-11-17 01:13:32 +0100

    merge lines

--- scripts/update-js-linting.sh
+++ scripts/update-js-linting.sh
@@ -47,8 +47,7 @@ else # Theme
 fi
 
 if [ -f "plugin.rb" ]; then
-  yarn ember-template-lint --fix --no-error-on-unmatched-pattern assets/javascripts || (echo "[update-js-linting] ember-template-lint failed, fix violations and re-run script" && exit 1)
-  yarn ember-template-lint --fix --no-error-on-unmatched-pattern admin/assets/javascripts || (echo "[update-js-linting] ember-template-lint failed, fix violations and re-run script" && exit 1)
+  yarn ember-template-lint --fix --no-error-on-unmatched-pattern 'assets/javascripts' 'admin/assets/javascripts' || (echo "[update-js-linting] ember-template-lint failed, fix violations and re-run script" && exit 1)
 else # Theme
   yarn ember-template-lint --fix --no-error-on-unmatched-pattern javascripts || (echo "[update-js-linting] ember-template-lint failed, fix violations and re-run script" && exit 1)
 fi
