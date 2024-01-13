1f493f7a84373087adaa55e2a88042a9de677645
Jarek Radosz <jradosz@gmail.com>
2024-01-13 02:11:08 +0100

    check for spec/ presence

--- scripts/update-rb-linting.sh
+++ scripts/update-rb-linting.sh
@@ -47,7 +47,7 @@ bundle lock --remove-platform arm64-darwin-21 &> /dev/null || true
 bundle lock --remove-platform arm64-darwin-22 &> /dev/null || true
 
 # Remove unnecessary requires
-find spec/ -name "*.rb" | xargs sed -i '' 's/require "rails_helper"//'
+test -d /spec && find spec/ -name "*.rb" | xargs sed -i '' 's/require "rails_helper"//'
 
 # Format and lint
 bundle exec stree write Gemfile $(git ls-files "*.rb") $(git ls-files "*.rake")
