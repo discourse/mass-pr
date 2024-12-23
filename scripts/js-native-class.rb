14fd9c66b3603507bac9321ddd75595fc33f8610
David Taylor <david@taylorhq.com>
2024-12-23 18:00:47 +0000

    upd

--- scripts/js-native-class.rb
+++ scripts/js-native-class.rb
@@ -40,7 +40,7 @@ else
   Dir["{test,javascripts}/**/*.js"]
 end
 
-if files.empty? || files.none? { |f| File.read(f).includes(".extend") }
+if (files.empty? || files.none? { |f| File.read(f).include?(".extend") }) && `git diff --name-only`.empty?
   puts "No files to transform"
   exit 0
 end
@@ -75,7 +75,6 @@ out = out.gsub(CONTROL_CHARS, "")
 changed = `git diff --name-only`
 if changed.empty?
   puts "[js-native-class] No files transformed"
-  exit 0
 else
   puts "[js-native-class] Files transformed:"
   puts changed
@@ -89,4 +88,11 @@ end
 if !out.match?(/\b0 errors/)
   puts "[js-native-class] Error in codemod. Fix it manually, then re-run to proceed"
   exit 1
+end
+
+files.each do |f|
+  if File.read(f).match?(/(observes|on).*discourse-common\/utils\/decorators/)
+    puts "[js-native-class] File #{f} uses observes from discourse-common. Update it."
+    exit 1
+  end
 end
