dc412b5816b54565f5a79c13945dfbc25ddbd2ec
Jarek Radosz <jarek@cvx.dev>
2025-02-19 18:44:49 +0100

    DEV: Update linting setup, add ruby linting and workflow (#31)

--- scripts/colocate-templates.rb
+++ scripts/colocate-templates.rb
@@ -1,11 +1,18 @@
 #!/usr/bin/env ruby
-require 'fileutils'
+# frozen_string_literal: true
+require "fileutils"
 
 Dir.chdir("repo")
 
 def is_core_override(component)
   if !File.exist?("../core")
-    system "git", "clone", "--depth", "1", "https://github.com/discourse/discourse", "../core", exception: true
+    system "git",
+           "clone",
+           "--depth",
+           "1",
+           "https://github.com/discourse/discourse",
+           "../core",
+           exception: true
   end
 
   Dir.glob("../core/**/components/#{component}.{hbs,js,gjs}").any?
@@ -16,47 +23,53 @@ def weird_js_path(component)
 end
 
 def any_imports_of(component)
-  Dir.glob("./**/*.{js,gjs}").any? do |path|
-    content = File.read(path)
-    content.include?("./#{component}") || content.include?("components/#{component}")
-  end
+  Dir
+    .glob("./**/*.{js,gjs}")
+    .any? do |path|
+      content = File.read(path)
+      content.include?("./#{component}") || content.include?("components/#{component}")
+    end
 end
 
-Dir.glob("**/{discourse,admin}/templates/components/**/*.hbs").each do |template_path|
-  component_name = template_path[/\/templates\/components\/(.+)\.hbs$/, 1]
-  if is_core_override(component_name)
-    puts "Skipping core override #{component_name}"
-    next
-  end
+Dir
+  .glob("**/{discourse,admin}/templates/components/**/*.hbs")
+  .each do |template_path|
+    component_name = template_path[%r{/templates/components/(.+)\.hbs$}, 1]
+    if is_core_override(component_name)
+      puts "Skipping core override #{component_name}"
+      next
+    end
 
-  destination = template_path.sub("/templates/components/", "/components/")
-  expected_js_path = destination.sub(/\.hbs$/, ".js")
+    destination = template_path.sub("/templates/components/", "/components/")
+    expected_js_path = destination.sub(/\.hbs$/, ".js")
 
-  puts "Moving #{template_path} to #{destination}"
-  FileUtils.mkdir_p(File.dirname(destination))
-  FileUtils.mv(template_path, destination)
+    puts "Moving #{template_path} to #{destination}"
+    FileUtils.mkdir_p(File.dirname(destination))
+    FileUtils.mv(template_path, destination)
 
-  if !File.exist?(expected_js_path)
-    if path = weird_js_path(component_name)
-      if any_imports_of(component_name)
-        raise "Help! A component is in a weird place, and it's being imported!"
-      end
-      puts "Moving #{path} to #{expected_js_path}..."
-      FileUtils.mv(path, expected_js_path)
-    else
-      puts "Creating #{expected_js_path}"
-      File.write(expected_js_path, <<~JS)
+    if !File.exist?(expected_js_path)
+      if path = weird_js_path(component_name)
+        if any_imports_of(component_name)
+          raise "Help! A component is in a weird place, and it's being imported!"
+        end
+        puts "Moving #{path} to #{expected_js_path}..."
+        FileUtils.mv(path, expected_js_path)
+      else
+        puts "Creating #{expected_js_path}"
+        File.write(expected_js_path, <<~JS)
         import Component from "@ember/component";
         export default class extends Component {}
       JS
+      end
     end
   end
-end
 
-Dir.glob("**/templates/connectors/**/*").each do |template_path|
-  next unless File.file?(template_path)
-  destination = template_path.sub("/templates/connectors/", "/connectors/")
-  puts "Moving #{template_path} to #{destination}"
-  FileUtils.mkdir_p(File.dirname(destination))
-  FileUtils.mv(template_path, destination)
-end
+Dir
+  .glob("**/templates/connectors/**/*")
+  .each do |template_path|
+    next unless File.file?(template_path)
+    destination = template_path.sub("/templates/connectors/", "/connectors/")
+    puts "Moving #{template_path} to #{destination}"
+    FileUtils.mkdir_p(File.dirname(destination))
+    FileUtils.mv(template_path, destination)
+  end
