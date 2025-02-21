7dc1b502e04ac3a216a21cd7378a2de4cba9e4bb
Jarek Radosz <jradosz@gmail.com>
2025-02-21 22:25:13 +0100

    sh to rb

---
+++ scripts/update_css_linting.rb
@@ -0,0 +1,21 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+Dir.chdir("repo")
+
+# Copy the config from skeleton if it does not already exist
+if !File.exist?("stylelint.config.mjs")
+  FileUtils.cp("../discourse-plugin-skeleton/stylelint.config.mjs", ".")
+end
+
+files =
+  if File.exist?("plugin.rb")
+    Dir["assets/**/*.scss"]
+  else
+    Dir["{javascripts,desktop,mobile,common,scss}/**/*.scss"]
+  end
+
+if !system "pnpm", "stylelint", "--fix", "--allow-empty-input", *files
+  puts "[update-css-linting] stylelint failed, fix violations and re-run script"
+  exit 1
+end
