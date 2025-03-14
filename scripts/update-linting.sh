affabef6c860674af6d11382e73b630b6ff4493c
Jarek Radosz <jarek@cvx.dev>
2025-03-14 01:57:06 +0100

    update workflows first

--- scripts/update-linting.sh
+++ scripts/update-linting.sh
@@ -2,9 +2,9 @@
 set -euxo pipefail
 
 ../scripts/ensure-minimum-scaffolding.sh
+../scripts/update-workflows.sh
 
 ../scripts/update-rb-linting.sh
 ../scripts/update-js-linting.sh
 ../scripts/update_css_linting.rb
-
-../scripts/update-workflows.sh
+../scripts/check_scss_deprecations.rb
